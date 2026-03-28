ff65ef5be3cc105d98e7394245a576b7279c2ea8
RICHABENIWAL <meitsjust017@gmail.com>
2026-03-28 12:38:28 +0530

    Update OkieDokie.java

--- src/test/java/com/test/Dumm/OkieDokie.java
+++ src/test/java/com/test/Dumm/OkieDokie.java
@@ -6,6 +6,10 @@ public class OkieDokie {
 @Test
 void test() {
 	System.out.println("Hello from Richa");
+	WebElement un=driver.findElement(by.id("username"));
+	WebElement pw=driver.findElement(by.id("password"));
+	WebElement login=driver.findElement(by.id("Login"));
+	
 }
 
 }
